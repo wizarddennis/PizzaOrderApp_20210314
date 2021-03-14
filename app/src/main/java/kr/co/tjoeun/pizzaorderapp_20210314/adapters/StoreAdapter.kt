d5be82d70b54a8bbc7efd86429cb459892c8f62b
dennis <travlInfo4you@gmail.com>
2021-03-14 12:20:44 +0900

    가게 로그 Glide로 불러내기

---
+++ app/src/main/java/kr/co/tjoeun/pizzaorderapp_20210314/adapters/StoreAdapter.kt
@@ -0,0 +1,42 @@
+package kr.co.tjoeun.pizzaorderapp_20210314.adapters
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.ImageView
+import android.widget.TextView
+import com.bumptech.glide.Glide
+import kotlinx.android.synthetic.main.store_list_item.view.*
+import kr.co.tjoeun.pizzaorderapp_20210314.R
+import kr.co.tjoeun.pizzaorderapp_20210314.datas.Store
+
+class StoreAdapter(val mContext : Context,
+                   val resId : Int,
+                   val mList : ArrayList<Store>) : ArrayAdapter<Store>(mContext, resId, mList) {
+
+    val inf = LayoutInflater.from(mContext)
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+        var tempRow = convertView
+
+        if(tempRow == null)
+        {
+            tempRow = inf.inflate(R.layout.store_list_item, null)
+        }
+
+        val row = tempRow!!
+
+        val storeData = mList[position]
+        val logoImg =row.findViewById<ImageView>(R.id.logoImg)
+        val storeNameTxt = row.findViewById<TextView>(R.id.storeNameTxt)
+
+        storeNameTxt.text = storeData.storeName
+
+        Glide.with(mContext).load(storeData.logoUrl).into(logoImg)
+
+        return row
+    }
+
+}
