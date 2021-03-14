61544ce0d4e7b92c8e6eafbe02b4f98fd6a6c877
dennis <travlInfo4you@gmail.com>
2021-03-14 12:37:14 +0900

    가게 상세화면 UI 작업

--- app/src/main/java/kr/co/tjoeun/pizzaorderapp_20210314/fragments/PizzaStoresFragment.kt
+++ app/src/main/java/kr/co/tjoeun/pizzaorderapp_20210314/fragments/PizzaStoresFragment.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import kotlinx.android.synthetic.main.fragment_pizza_stores.*
 import kr.co.tjoeun.pizzaorderapp_20210314.R
 import kr.co.tjoeun.pizzaorderapp_20210314.adapters.StoreAdapter
 import kr.co.tjoeun.pizzaorderapp_20210314.datas.Store
