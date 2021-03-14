ae4e923993fb343b5aa1bfc47dd4736896ba9287
dennis <travlInfo4you@gmail.com>
2021-03-14 10:42:47 +0900

    Initial commit

---
+++ app/src/main/java/kr/co/tjoeun/pizzaorderapp_20210314/MainActivity.kt
@@ -0,0 +1,20 @@
+package kr.co.tjoeun.pizzaorderapp_20210314
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : BaseActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    override fun setupEvents() {
+
+    }
+
+    override fun setValues() {
+
+    }
+}
