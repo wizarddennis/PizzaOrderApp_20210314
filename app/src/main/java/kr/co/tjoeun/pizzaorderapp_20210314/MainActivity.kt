5f9e9d3428101c7752635912d962bda2a8c7c636
dennis <travlInfo4you@gmail.com>
2021-03-14 10:45:33 +0900

    BaseActivity를 상속받도록 MainActivity.kt 변경

--- app/src/main/java/kr/co/tjoeun/pizzaorderapp_20210314/MainActivity.kt
+++ app/src/main/java/kr/co/tjoeun/pizzaorderapp_20210314/MainActivity.kt
@@ -8,6 +8,9 @@ class MainActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setupEvents()
+        setValues()
     }
 
     override fun setupEvents() {
