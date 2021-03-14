0edc6c40b5cbb05353817646f56aa4db80ec0aca
dennis <travlInfo4you@gmail.com>
2021-03-14 14:09:51 +0900

    입력 후 확인 누르면 복귀 처리

--- app/src/main/java/kr/co/tjoeun/pizzaorderapp_20210314/EditNicknameActivity.kt
+++ app/src/main/java/kr/co/tjoeun/pizzaorderapp_20210314/EditNicknameActivity.kt
@@ -1,15 +1,30 @@
 package kr.co.tjoeun.pizzaorderapp_20210314
 
+import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_edit_nickname.*
 
 class EditNicknameActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_edit_nickname)
+
+        setupEvents()
+        setValues()
     }
 
     override fun setupEvents() {
+        okBtn.setOnClickListener {
+            val inputNickname = nicknameEdt.text.toString()
+
+            val resultIntent = Intent()
+            resultIntent.putExtra("nick", inputNickname)
+            setResult(Activity.RESULT_OK, resultIntent)
+
+            finish()
+        }
 
     }
 
